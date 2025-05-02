e02b124a1f43228c907d6927eee2bc8827a79522
zerrocredit <118961963+zerrocredit@users.noreply.github.com>
2025-05-02 00:00:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ static/assets/js/i.js
@@ -0,0 +1,76 @@
+window.addEventListener("load", () => {
+  navigator.serviceWorker.register("../sw.js?v=10-02-2024", {
+    scope: "/a/",
+  });
+});
+
+let xl;
+
+try {
+  xl = window.top.location.pathname === "/rx";
+} catch {
+  try {
+    xl = window.parent.location.pathname === "/rx";   
+  } catch {
+    xl = false;
+  }
+}
+
+const form = document.getElementById("fv");
+const input = document.getElementById("iv");
+
+if (form && input) {
+  form.addEventListener("submit", async event => {
+    event.preventDefault();
+    try {
+      if (xl) processUrl(input.value, "");
+      else processUrl(input.value, "/rx");
+    } catch {
+      processUrl(input.value, "/rx");
+    }
+  });
+}
+function processUrl(value, path) {
+  let url = value.trim();
+  const engine = localStorage.getItem("engine");
+  const searchUrl = engine ? engine : "https://www.google.com/search?q=";
+
+  if (!isUrl(url)) {
+    url = searchUrl + url;
+  } else if (!(url.startsWith("https://") || url.startsWith("http://"))) {
+    url = `https://${url}`;
+  }
+
+  sessionStorage.setItem("GoUrl", __uv$config.encodeUrl(url));
+  const dy = localStorage.getItem("dy");
+
+  if (dy === "true") {
+    window.location.href = `/a/q/${__uv$config.encodeUrl(url)}`;
+  } else if (path) {
+    location.href = path;
+  } else {
+    window.location.href = `/a/${__uv$config.encodeUrl(url)}`;
+  }
+}
+
+function go(value) {
+  processUrl(value, "/rx");
+}
+
+function blank(value) {
+  processUrl(value);
+}
+
+function dy(value) {
+  processUrl(value, `/a/q/${__uv$config.encodeUrl(value)}`);
+}
+
+function isUrl(val = "") {
+  if (
+    /^http(s?):\/\//.test(val) ||
+    (val.includes(".") && val.substr(0, 1) !== " ")
+  ) {
+    return true;
+  }
+  return false;
+}
